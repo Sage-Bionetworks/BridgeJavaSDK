5baa8b8a2da0a7fb25663ff3bc30ee7a0a3bf4b1
Chris <christopher.engelbrecht@sagebase.org>
2014-09-12 16:26:56 -0700

    first commit of maven project. Maven project is simply "hello world".

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bridge Java SDK
+This is the Java SDK for the Bridge Server (http://github.com/Sage-Bionetworks/BridgePF). This is Maven project used for builds.
---
+++ src/main/java/org/sagebionetworks/bridge/sdk/App.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.bridge.sdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
