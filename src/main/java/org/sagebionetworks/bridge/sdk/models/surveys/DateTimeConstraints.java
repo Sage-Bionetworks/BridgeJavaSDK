0e9bf64539e4465ad5d00cff6331a5841b1a7925
Alx Dark <alx.dark@sagebase.org>
2014-11-04 13:44:48 -0800

    Updates to include new date-based constraints

--- src/main/java/org/sagebionetworks/bridge/sdk/models/surveys/DateTimeConstraints.java
+++ src/main/java/org/sagebionetworks/bridge/sdk/models/surveys/DateTimeConstraints.java
@@ -1,9 +1,13 @@
 package org.sagebionetworks.bridge.sdk.models.surveys;
 
+import org.joda.time.DateTime;
+
 
 public class DateTimeConstraints extends Constraints {
 
     private boolean allowFuture = false;
+    private DateTime earliestValue;
+    private DateTime latestValue;
 
     public DateTimeConstraints() {
         setDataType(DataType.DATETIME);
@@ -14,5 +18,17 @@ public class DateTimeConstraints extends Constraints {
     }
     public void setAllowFuture(boolean allowFuture) {
         this.allowFuture = allowFuture;
+    }
+    public DateTime getEarliestValue() {
+        return earliestValue;
+    }
+    public void setEarliestValue(DateTime earliestValue) {
+        this.earliestValue = earliestValue;
+    }
+    public DateTime getLatestValue() {
+        return latestValue;
+    }
+    public void setLatestValue(DateTime latestValue) {
+        this.latestValue = latestValue;
     }
 }
