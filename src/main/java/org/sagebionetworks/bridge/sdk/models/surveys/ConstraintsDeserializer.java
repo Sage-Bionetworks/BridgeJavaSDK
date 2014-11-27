d65392455178f242bcacdbab5b2d6ddcf7e955f5
Alx Dark <alx.dark@sagebase.org>
2014-11-26 16:05:37 -0800

    removing imports

--- src/main/java/org/sagebionetworks/bridge/sdk/models/surveys/ConstraintsDeserializer.java
+++ src/main/java/org/sagebionetworks/bridge/sdk/models/surveys/ConstraintsDeserializer.java
@@ -4,15 +4,11 @@ import java.io.IOException;
 
 import org.sagebionetworks.bridge.sdk.Utilities;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class ConstraintsDeserializer extends JsonDeserializer<Constraints> {
