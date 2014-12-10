f9dd358d60189045ed3c29e3d6aca13e9b9b86e4
Alx Dark <alx.dark@sagebase.org>
2014-12-10 10:51:35 -0800

    Adding tests for researcher permissions

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/StudyTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/StudyTest.java
@@ -6,12 +6,15 @@ import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.sagebionetworks.bridge.Tests;
 import org.sagebionetworks.bridge.sdk.AdminClient;
+import org.sagebionetworks.bridge.sdk.ResearcherClient;
 import org.sagebionetworks.bridge.sdk.TestUserHelper;
 import org.sagebionetworks.bridge.sdk.TestUserHelper.TestUser;
 import org.sagebionetworks.bridge.sdk.exceptions.EntityNotFoundException;
+import org.sagebionetworks.bridge.sdk.exceptions.UnauthorizedException;
 import org.sagebionetworks.bridge.sdk.models.holders.VersionHolder;
 import org.sagebionetworks.bridge.sdk.models.studies.Study;
 
@@ -21,26 +24,25 @@ public class StudyTest {
     
     private TestUser admin;
     
-    private String identifier;
-
+    private Study study;
+    
     @Before
     public void before() {
-        identifier = Tests.randomIdentifier();
         admin = TestUserHelper.getSignedInAdmin();
     }
 
     @After
     public void after() {
-        if (identifier != null) {
-            admin.getSession().getAdminClient().deleteStudy(identifier);
+        if (study != null) {
+            admin.getSession().getAdminClient().deleteStudy(study.getIdentifier());
         }
         admin.getSession().signOut();
     }
 
     @Test
     public void crudStudy() throws Exception {
-        Study study = new Study();
-        study.setIdentifier(identifier);
+        study = new Study();
+        study.setIdentifier(Tests.randomIdentifier());
         study.setMinAgeOfConsent(18);
         study.setMaxNumOfParticipants(100);
         study.setName("Test Study [SDK]");
@@ -71,7 +73,7 @@ public class StudyTest {
         assertEquals(50, newStudy.getMaxNumOfParticipants());
 
         client.deleteStudy(newStudy.getIdentifier());
-        identifier = null;
+        study = null;
         
         try {
             newStudy = client.getStudy(study.getIdentifier());
@@ -80,9 +82,38 @@ public class StudyTest {
         }
     }
 
-    // Testing a researcher requires that the host be hacked so that the researcher matches
-    // the host name of this newly created study. That's beyond the debugging setup of the 
-    // server and the SDK at the moment. Would need to set a header, I think, with the host
-    // that came back in the study.
+    @Test
+    public void researcherCannotAccessAnotherStudy() {
+        study = new Study();
+        study.setIdentifier(Tests.randomIdentifier());
+        study.setMinAgeOfConsent(18);
+        study.setMaxNumOfParticipants(100);
+        study.setName("Test Study [SDK]");
+        study.setTrackers(Lists.newArrayList("sage:A", "sage:B"));
+        
+        AdminClient client = admin.getSession().getAdminClient();        
+        client.createStudy(study);
+        
+        TestUser researcher = TestUserHelper.createAndSignInUser(StudyTest.class, false, Tests.TEST_KEY+"_researcher");
+        try {
+            researcher.getSession().getAdminClient().getStudy(study.getIdentifier());
+            fail("Should not have been able to get this other study");
+        } catch(UnauthorizedException e) {
+            assertEquals("Unauthorized HTTP response code", 403, e.getStatusCode());
+        } finally {
+            researcher.signOutAndDeleteUser();
+        }
+        
+    }
+    
+    @Test
+    public void researcherCanAccessStudy() {
+        TestUser researcher = TestUserHelper.createAndSignInUser(StudyTest.class, false, Tests.TEST_KEY+"_researcher");
+        
+        ResearcherClient rclient = researcher.getSession().getResearcherClient();
+        Study serverStudy = rclient.getStudy();
+        
+        assertEquals(Tests.TEST_KEY+"_researcher", serverStudy.getResearcherRole());
+    }
     
 }
