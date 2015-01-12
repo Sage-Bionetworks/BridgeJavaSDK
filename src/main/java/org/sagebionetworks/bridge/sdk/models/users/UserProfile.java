b8b3344cc7a1ba6c27937afa6b8b41b275364e6a
alxdarksage <alx.dark@sagebase.org>
2015-01-12 13:38:01 -0800

    You can add a phone to your user profile.

--- src/main/java/org/sagebionetworks/bridge/sdk/models/users/UserProfile.java
+++ src/main/java/org/sagebionetworks/bridge/sdk/models/users/UserProfile.java
@@ -8,19 +8,22 @@ public final class UserProfile {
     private String firstName;
     private String lastName;
     private String username;
+    private String phone;
     private String email;
 
     @JsonCreator
     private UserProfile(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName,
-            @JsonProperty("username") String username, @JsonProperty("email") String email) {
+            @JsonProperty("username") String username, @JsonProperty("email") String email,
+            @JsonProperty("phone") String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.email = email;
+        this.phone = phone;
     }
     
     public static UserProfile valueOf() {
-        return new UserProfile(null, null, null, null);
+        return new UserProfile(null, null, null, null, null);
     }
 
     public String getFirstName() {
@@ -38,6 +41,10 @@ public final class UserProfile {
     public String getEmail() {
         return email;
     }
+    
+    public String getPhone() {
+        return phone;
+    }
 
     public UserProfile setFirstName(String firstName) {
         this.firstName = firstName;
@@ -48,13 +55,16 @@ public final class UserProfile {
         this.lastName = lastName;
         return this;
     }
+    
+    public UserProfile setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
 
     @Override
     public String toString() {
-        return "UserProfile[firstName=" + firstName + ", "
-                + "lastName=" + lastName + ", "
-                + "username=" + username + ", "
-                + "email=" + email + "]";
+        return "UserProfile[firstName=" + firstName + ", " + "lastName=" + lastName + ", " + "username=" + username
+                + ", " + "phone=" + phone + ", " + "email=" + email + "]";
     }
 
 }
