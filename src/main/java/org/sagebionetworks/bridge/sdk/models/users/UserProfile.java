6e409ec9695ab948de5b1740bd0651df6ae34559
alxdarksage <alx.dark@sagebase.org>
2015-01-13 09:40:03 -0800

    Removing valueOf()... TODO: All the other model classes --> simple java beans

--- src/main/java/org/sagebionetworks/bridge/sdk/models/users/UserProfile.java
+++ src/main/java/org/sagebionetworks/bridge/sdk/models/users/UserProfile.java
@@ -1,8 +1,5 @@
 package org.sagebionetworks.bridge.sdk.models.users;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public final class UserProfile {
 
     private String firstName;
@@ -11,60 +8,40 @@ public final class UserProfile {
     private String phone;
     private String email;
 
-    @JsonCreator
-    private UserProfile(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName,
-            @JsonProperty("username") String username, @JsonProperty("email") String email,
-            @JsonProperty("phone") String phone) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.email = email;
-        this.phone = phone;
-    }
-    
-    public static UserProfile valueOf() {
-        return new UserProfile(null, null, null, null, null);
-    }
-
     public String getFirstName() {
         return firstName;
     }
-
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
     public String getLastName() {
         return lastName;
     }
-
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     public String getUsername() {
         return username;
     }
-
-    public String getEmail() {
-        return email;
+    public void setUsername(String username) {
+        this.username = username;
     }
-    
     public String getPhone() {
         return phone;
     }
-
-    public UserProfile setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
-
-    public UserProfile setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
     
-    public UserProfile setPhone(String phone) {
-        this.phone = phone;
-        return this;
-    }
-
     @Override
     public String toString() {
-        return "UserProfile[firstName=" + firstName + ", " + "lastName=" + lastName + ", " + "username=" + username
-                + ", " + "phone=" + phone + ", " + "email=" + email + "]";
+        return "UserProfile [firstName=" + firstName + ", lastName=" + lastName + 
+                ", username=" + username + ", phone=" + phone + ", email=" + email + "]";
     }
-
 }
