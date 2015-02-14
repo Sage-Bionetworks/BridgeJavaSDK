287a80b272c696cc7891623efcc984d0bd93a0d0
Dwayne Jeng <dwayne.jeng@sagebase.org>
2015-02-13 16:33:59 -0800

    delete deprecated Tracker and Health Data v1 objects and APIs

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/UTF8Test.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/UTF8Test.java
@@ -2,7 +2,6 @@ package org.sagebionetworks.bridge.sdk.integration;
 
 import static org.junit.Assert.assertEquals;
 
-import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,16 +10,8 @@ import org.sagebionetworks.bridge.sdk.Session;
 import org.sagebionetworks.bridge.sdk.TestUserHelper;
 import org.sagebionetworks.bridge.sdk.TestUserHelper.TestUser;
 import org.sagebionetworks.bridge.sdk.UserClient;
-import org.sagebionetworks.bridge.sdk.models.ResourceList;
-import org.sagebionetworks.bridge.sdk.models.holders.GuidVersionHolder;
-import org.sagebionetworks.bridge.sdk.models.studies.Tracker;
-import org.sagebionetworks.bridge.sdk.models.users.HealthDataRecord;
 import org.sagebionetworks.bridge.sdk.models.users.SignInCredentials;
 import org.sagebionetworks.bridge.sdk.models.users.UserProfile;
-
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.collect.Lists;
 
 public class UTF8Test {
     
@@ -28,40 +19,14 @@ public class UTF8Test {
     
     @Before
     public void before() {
-        testUser = TestUserHelper.createAndSignInUser(HealthDataTest.class, true);
+        testUser = TestUserHelper.createAndSignInUser(UTF8Test.class, true);
     }
     
     @After
     public void after() {
         testUser.signOutAndDeleteUser();
     }
-    
-    @Test
-    public void canSaveAndRetrieveUTF8HealthData() {
-        UserClient client = testUser.getSession().getUserClient();
-        
-        ObjectNode data = JsonNodeFactory.instance.objectNode();
-        data.put("medication", "☃");
-        data.put("dosage", "♘");
-        data.put("frequency", "советских военных судов и самолетов была отмечена");
-        
-        HealthDataRecord record = new HealthDataRecord();
-        record.setData(data);
-        record.setStartDate(DateTime.now());
-        record.setEndDate(DateTime.now());
-        
-        // Medication tracker, we hope.
-        Tracker tracker = testUser.getSession().getUserClient().getAllTrackers().getItems().get(1); 
-        
-        ResourceList<GuidVersionHolder> guids = client.addHealthDataRecords(tracker, Lists.newArrayList(record));
-        
-        record = client.getHealthDataRecord(tracker, guids.get(0).getGuid());
-        
-        assertEquals("☃", record.getData().get("medication").asText());
-        assertEquals("♘", record.getData().get("dosage").asText());
-        assertEquals("советских военных судов и самолетов была отмечена", record.getData().get("frequency").asText());
-    }
-    
+
     @Test
     public void canSaveAndRetrieveDataStoredInRedis() {
         UserClient client = testUser.getSession().getUserClient();
