308fa9d4dec82349901b99bbfb64d3ba5ac145b5
alxdarksage <alx.dark@sagebase.org>
2015-02-23 12:55:48 -0800

    Adding test for accounts changes; change to verify environment is correct on Travis

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/AuthenticationTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/AuthenticationTest.java
@@ -1,11 +1,18 @@
 package org.sagebionetworks.bridge.sdk.integration;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.sagebionetworks.bridge.sdk.AdminClient;
 import org.sagebionetworks.bridge.sdk.ClientProvider;
 import org.sagebionetworks.bridge.sdk.Config;
+import org.sagebionetworks.bridge.sdk.Config.Props;
 import org.sagebionetworks.bridge.sdk.Session;
 import org.sagebionetworks.bridge.sdk.TestUserHelper;
+import org.sagebionetworks.bridge.sdk.TestUserHelper.TestUser;
 import org.sagebionetworks.bridge.sdk.exceptions.BridgeServerException;
+import org.sagebionetworks.bridge.sdk.models.studies.Study;
+import org.sagebionetworks.bridge.sdk.models.users.SignInCredentials;
 import org.sagebionetworks.bridge.sdk.models.users.SignUpCredentials;
 
 public class AuthenticationTest {
@@ -36,4 +43,29 @@ public class AuthenticationTest {
         ClientProvider.resendEmailVerification("fooboo-sagebridge@antwerp.com");
     }
     
+    @Test
+    public void accountWithOneStudySeparateFromAccountWithSecondStudy() {
+        TestUser testUser1 = TestUserHelper.createAndSignInUser(ConsentTest.class, true);
+        Config config = ClientProvider.getConfig();
+        AdminClient client = ClientProvider.signIn(config.getAdminCredentials()).getAdminClient();
+        try {
+            // Make a second study for this test:
+            Study study = new Study();
+            study.setIdentifier("secondstudy");
+            study.setName("Second Study");
+            client.createStudy(study);
+            
+            // Can we sign in to secondstudy? No.
+            try {
+                config.set(Props.STUDY_IDENTIFIER, "secondstudy");
+                ClientProvider.signIn(new SignInCredentials(testUser1.getUsername(), testUser1.getPassword()));
+                fail("Should not have allowed sign in");
+            } catch(BridgeServerException e) {
+                assertEquals(404, e.getStatusCode());
+            }
+        } finally {
+            client.deleteStudy("secondstudy");
+            testUser1.signOutAndDeleteUser();
+        }
+    }
 }
