34472674a8966b35e68d65310e4ee0acd515d151
alxdarksage <alx.dark@sagebase.org>
2015-03-12 15:16:22 -0700

    Remove import

--- src/test/java/org/sagebionetworks/bridge/sdk/ClientProviderTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/ClientProviderTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.sagebionetworks.bridge.Tests;
 import org.sagebionetworks.bridge.sdk.TestUserHelper.TestUser;
