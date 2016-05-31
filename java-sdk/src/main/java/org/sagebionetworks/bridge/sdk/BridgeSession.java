012d257234b3fe76c1e86f453cec093d91204f7b
alxdarksage <alx.dark@sagebase.org>
2016-05-31 15:04:34 -0700

    Adding request reset password feature for researchers.

--- java-sdk/src/main/java/org/sagebionetworks/bridge/sdk/BridgeSession.java
+++ java-sdk/src/main/java/org/sagebionetworks/bridge/sdk/BridgeSession.java
@@ -10,7 +10,6 @@ import org.sagebionetworks.bridge.sdk.models.subpopulations.ConsentStatus;
 import org.sagebionetworks.bridge.sdk.models.subpopulations.SubpopulationGuid;
 import org.sagebionetworks.bridge.sdk.models.users.SharingScope;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.google.common.collect.ImmutableMap;
 
 class BridgeSession implements Session {
