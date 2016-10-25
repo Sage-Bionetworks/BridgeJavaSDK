799441fd742566d37b4949491699b6efd89caa32
alxdarksage <alx.dark@sagebase.org>
2016-10-25 13:38:24 -0700

    Updates based on integration tests (e.g. changing handling of Joda library to support time zones correctly).

--- rest-client/src/main/java/org/sagebionetworks/bridge/sdk/rest/ApiClientProvider.java
+++ rest-client/src/main/java/org/sagebionetworks/bridge/sdk/rest/ApiClientProvider.java
@@ -1,34 +1,33 @@
 package org.sagebionetworks.bridge.sdk.rest;
 
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Type;
 import java.util.Map;
 
-import com.fatboyindustrial.gsonjodatime.Converters;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
-import com.google.common.io.BaseEncoding;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import org.sagebionetworks.bridge.sdk.rest.gson.ByteArrayToBase64TypeAdapter;
+import org.sagebionetworks.bridge.sdk.rest.gson.DateTimeTypeAdapter;
+import org.sagebionetworks.bridge.sdk.rest.gson.LocalDateTypeAdapter;
 import org.sagebionetworks.bridge.sdk.rest.model.SignIn;
 
 /**
  * Created by liujoshua on 10/11/16.
  */
 public class ApiClientProvider {
-    public static final Gson GSON = Converters.registerAll(new GsonBuilder())
-            .registerTypeAdapter(byte[].class, new ByteArrayToBase64TypeAdapter()).create();
+    
+    public static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(byte[].class, new ByteArrayToBase64TypeAdapter())
+            .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+            .registerTypeAdapter(DateTime.class, new DateTimeTypeAdapter()).create();
     
     private final OkHttpClient unauthenticatedOkHttpClient;
     private final Retrofit.Builder retrofitBuilder;
@@ -148,20 +147,5 @@ public class ApiClientProvider {
     // allow test access to retrofit builder
     Retrofit.Builder getRetrofitBuilder() {
         return retrofitBuilder;
-    }
-
-    private static class ByteArrayToBase64TypeAdapter implements JsonSerializer<byte[]>,
-            JsonDeserializer<byte[]> {
-        public byte[] deserialize(
-                JsonElement json,
-                Type typeOfT,
-                JsonDeserializationContext context
-        ) throws JsonParseException {
-            return BaseEncoding.base64().decode(json.getAsString());
-        }
-
-        public JsonElement serialize(byte[] src, Type typeOfSrc, JsonSerializationContext context) {
-            return new JsonPrimitive(BaseEncoding.base64().encode(src));
-        }
     }
 }
