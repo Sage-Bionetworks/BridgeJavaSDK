61d882b69b9142a9521cafbc82c0dde8d859fd15
zaclinsage <zhizhen.lin@sagebase.org>
2016-10-24 17:48:56 -0700

    add hashcode(), equal() and toString() methods to ReportData entity

--- java-sdk/src/main/java/org/sagebionetworks/bridge/sdk/models/reports/ReportData.java
+++ java-sdk/src/main/java/org/sagebionetworks/bridge/sdk/models/reports/ReportData.java
@@ -1,10 +1,16 @@
 package org.sagebionetworks.bridge.sdk.models.reports;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.joda.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.sagebionetworks.bridge.sdk.models.upload.Upload;
+
+import java.util.Objects;
+
+import static org.sagebionetworks.bridge.sdk.utils.BridgeUtils.TO_STRING_STYLE;
 
 /**
  * Record representing the data for a report entry (for a specific day). The SDK expects this data 
@@ -28,5 +34,25 @@ public class ReportData {
     @JsonProperty("data")
     public JsonNode getReportData() {
         return reportData;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, reportData);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        ReportData other = (ReportData) obj;
+        return Objects.equals(date, other.date)
+                && Objects.equals(reportData, other.reportData);
+    }
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, TO_STRING_STYLE).append("date", date)
+                .append("reportData", reportData).build();
     }
 }
