40467cc400cc1201cbd7d42088d7888805de0d01
Joshua Liu <joshua.liu@sagebase.org>
2017-06-01 10:27:59 -0700

    Update ExternalIdList from swagger.json

--- rest-client/src/test/java/org/sagebionetworks/bridge/rest/gson/LocalDateTypeAdapterTest.java
+++ rest-client/src/test/java/org/sagebionetworks/bridge/rest/gson/LocalDateTypeAdapterTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.verify;
 
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
